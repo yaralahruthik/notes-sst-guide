da6a55409324871933d01861d858afad37ae69b0
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-13 11:32:22 +0530

    checkpoint

--- sst.config.ts
+++ sst.config.ts
@@ -3,14 +3,14 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
+      name: 'notes',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
     };
   },
   async run() {
-    await import("./infra/storage");
-    const api = await import("./infra/api");
+    await import('./infra/storage');
+    const api = await import('./infra/api');
 
     return {
       api: api.myApi.url,
